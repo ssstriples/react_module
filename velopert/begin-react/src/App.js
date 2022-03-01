805ffdec9b0799f8fb429bbf8bf51efd10fd57f8
lsh_gram <jplsh76032004@gmail.com>
2022-03-01 21:46:50 +0900

    [React][Velopert] 배열 삭제/수정, useEffect
    <DESC>
    - 배열에 항목 제거
    - 배열에 항목 수정
    - useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -34,17 +34,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -63,6 +66,20 @@ function App() {
       email: ''
     });
     nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬 
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
   };
 
   return (
@@ -91,7 +108,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
